154dd3fcf37893486d036c0905d58f05b79eca68
AparinAA <alxaparin@gmail.com>
2023-05-23 21:12:26 +0300

    add lib ramda and function prog

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,33 +1,46 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { tryCatch } from 'ramda';
-
 import addArticle from './helpfunctionController/addArticle.js';
 import editArticle from './helpfunctionController/editArticle.js';
 import deleteArticle from './helpfunctionController/deleteArticle.js';
-
-const createSaveFunction = (fn, errorClass) => tryCatch(fn, errorClass);
+import getArticles from './helpfunctionController/getArticles.js';
 
 function Controller() {
     //Controller to add the new article
     async function addArticleSave(req, res, next) {
-        return createSaveFunction(addArticle, next);
+        try {
+            addArticle(req, res);
+        } catch (err) {
+            next(err);
+        }
     }
 
     //Controller to edit the article
     async function editArticleSave(req, res, next) {
-        return createSaveFunction(editArticle, next);
+        try {
+            await editArticle(req, res);
+        } catch (err) {
+            next(err);
+        }
     }
 
     //Controller to delete the article
     async function deleteArticleSave(req, res, next) {
-        return createSaveFunction(deleteArticle, next);
+        try {
+            await deleteArticle(req, res);
+        } catch (err) {
+            next(err);
+        }
     }
 
     //Controller to get articles
     async function getArticlesSave(req, res, next) {
-        return createSaveFunction(getArticles, next);
+        try {
+            await getArticles(req, res);
+        } catch (err) {
+            next(err);
+        }
     }
 
     return {
--- src/controllers/helpfunctionController/getArticles.js
+++ src/controllers/helpfunctionController/getArticles.js
@@ -24,7 +24,6 @@ const getParmsOfPage = applySpec({
     page: getPageInt
 });
 
-
 const getArticles = async (req, res) => {
     const { limit, page } = getParmsOfPage(req.query);
     const articles = await db.collection('myCollection').find({}).toArray();
@@ -33,33 +32,36 @@ const getArticles = async (req, res) => {
         throw new BadRequestError('Something went wrong. Articles are not found');
     }
 
+    if (limit < 1 || page < 1) {
+        throw new BadRequestError('Params must to be greater than 1');
+    }
+
     let resultRequest = {
         _links: {
-            "base": "/api/articles",
-            "context": "",
             "next": null,
             "prev": null,
-            "self": `/api/articles`
+            "self": `/api/articles`,
         },
         page: 1,
         allPages: 1,
-    };
+        data: [],
+    }
 
-    if (limit && page) {
+    if (page && limit) {
         const allPages = Math.ceil(articles.length / limit);
 
-        resultRequest = {
-            _links: {
-                "next": predictHelpUrl(page < allPages, page + 1, limit),
-                "prev": predictHelpUrl(page > 1, page - 1, limit),
-                "self": `/api/articles?page=${page}&limit=${limit}`
-            },
-            page,
-            allPages,
-            data: articles.slice((page - 1) * limit, start + limit),
-        }
+        resultRequest._links = {
+            "next": predictHelpUrl(page < allPages, page + 1, limit),
+            "prev": predictHelpUrl(page > 1, page - 1, limit),
+            "self": `/api/articles?page=${page}&limit=${limit}`
+        };
+
+        resultRequest.page = page;
+        resultRequest.allPages = allPages;
+        resultRequest.data = articles.slice((page - 1) * limit, page * limit);
+
     } else {
-        resultRequest = { data: articles }
+        resultRequest.data = articles;
     }
 
     return res.json(resultRequest);
