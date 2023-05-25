bd29b1d50c049337f21e6922f8627c62e8cf21f0
AparinAA <alxaparin@gmail.com>
2023-05-25 13:17:33 +0300

    update auth on the function par

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,20 +1,41 @@
 import jwt from 'jsonwebtoken';
 import { UnauthorizationError } from '../module/error.js';
+import { allPass, complement, compose, curry, isNil, prop, startsWith, slice, partial, ifElse, applySpec, partialRight, tap } from 'ramda';
+
+//get token from headers
+const getHeaders = prop('headers');
+const getAuthrization = prop('authorization');
+const getAuthHeader = compose(getAuthrization, getHeaders); // main get header
+
+//is correct token
+const isAuthHeader = complement(isNil);
+const startsWithBearer = curry(startsWith)('Bearer ');
+const checkAuthHeader = allPass([isAuthHeader, startsWithBearer]);
+const getAccessToken = partial(slice, [7, Infinity]);
+const getAndCheckAuthHeader = compose(checkAuthHeader, getAuthHeader); //main check
+
+//to decode token
+const partialJwtVerify = partialRight(jwt.verify, [process.env.SECRET_PRIVATE_KEY]);
+const jwtVerify = compose(partialJwtVerify, getAccessToken);
+
+//user data generation
+const makeUserData = applySpec({
+    uid: compose(prop('id'), jwtVerify)
+});
+const getTokenAndMakeUserDate = compose(makeUserData, getAuthHeader); //main make
+
+//if else check correct token
+const ifElseCheck = ifElse(
+    getAndCheckAuthHeader,
+    getTokenAndMakeUserDate,
+    tap(() => { throw new UnauthorizationError('User is not authorization') })
+)
 
 // middleware to check authorization
 export default function (req, res, next) {
     try {
-        const authHeader = req.headers.authorization;
-        if (authHeader && authHeader.startsWith('Bearer ')) {
-            const accessToken = authHeader.slice(7, authHeader.length);
-
-            const decodeToken = jwt.verify(accessToken, process.env.SECRET_PRIVATE_KEY);
-            req.user = { uid: decodeToken.id };
-
-            next();
-        } else {
-            throw new UnauthorizationError('User is not authorization');
-        }
+        req.user = ifElseCheck(req);
+        next();
     } catch (err) {
         next(err);
     }
